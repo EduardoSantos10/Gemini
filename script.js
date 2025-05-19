334becb3e4cf8b349e66823599ef1929b060d6d9
Eduardo <eduardo25082002@gmail.com>
2025-05-19 17:12:29 -0300

    Quinto Commit

--- script.js
+++ script.js
@@ -125,4 +125,105 @@ incomingMessageElement, skipEffect = false) => {
     }, 75);
 };
 
+// Fetch Api Response
+
+const requestApiResponse = async (incomingMessageElement) => {
+    const messageElement = incomingMessageElement.querySelector(".message__text");
+
+    try{
+        const response = await fetch(API_REQUEST_URL, {
+            method: "POST",
+            headers: {"Content-Type": "application/json" },
+            body: JSON.stringify({
+                contents: [{ role: "user", parts: [{ text: 
+                currentUserMessage }] }]
+            }),
+        });
+
+        const responseData = await response.json();
+        if (!response.ok) throw new Error(responseData.error.message);
+
+        const responseText = responseData?.conditates?.[0]?.content?.
+        parts?.[0]?.text;
+        if (!responseText) throw new Error("Invalid API Response,");
+
+        const parsedApiResponse = marked.parse(responseText);
+        const rawApiResponse = responseText;
+
+        showTypingEffect(rawApiResponse, parsedApiResponse,
+        messageTextElement, incomingMessageElement);
+
+        // Save conversation
+
+        let savedConversations = JSON.parse(localStorage.getItem
+            ("saved-api-cheats")) || [];
+            savedConversations.push({
+            userMessage: currentUserMessage,
+            apiResponse: responseData
+        });
+        localStorage.setItem("saved-api-chats", JSON.stringify
+        (savedConversations));
+        
+    }catch(error){
+        isGeneratingResponse = false;
+        messageTextElement.innerText = error.message;
+        messageTextElement.closest(".message").classList.add
+        ("message__error");
+    } finally{
+        incomingMessageElement.classList.remove("message--loading");
+    }
+};
+
+// Add copy button
+
+const addCopyButtonToCodeBlocks = () =>{
+    const codeBlocks = document.querySelectorAll('pre');
+    codeBlocks.forEach((block)=>{
+        const codeElement = block.querySelector('code');
+        let language = [...codeElement.classList].find(cls => cls.
+        startsWith('language-'))?.replace('language-', '') || 'Text';
+
+        const languageLabel = document.createElement('div');
+        languageLabel.innerText = language.charAt(0).toUpperCase() + 
+        language.slice(1);
+        languageLabel.classList.add('code__language-label');
+        block.appendChild(languageLabel);
+
+        const copyButton = document.createElement('button');
+        copyButton.innerHTML = `<i class='bx bx-copy'></i>`;
+        copyButton.classList.add('code__copy-btn');
+        block.appendChild(copyButton);
+
+        copyButton.addEventListener('click', ()=>{
+            navigator.clipboard.writeText(codeElement.innerText).then(() => {
+                copyButton.innerHTML = `<i class='bx bx-check'></i>`;
+                setTimeout(() => copyButton.innerHTML = `<i class='bx bx-copy'></i>`, 2000);
+            }).catch(err => {
+                console.error("Copy failed:", err);
+            });
+        });
+    });
+};
+
+// Show loading animation
+
+const displayLoadingAnimation = () =>{
+    const loadingHtml = `
+    
+        <div class="message__content">
+            <img class="message__avatar" src="assets/gemini.svg"
+            alt="Gemini avatar">
+            <p class="message__text"></p>
+            <div class="message__loading-indicator">
+                <div class="message__loading-bar"></div>
+                <div class="message__loading-bar"></div>
+                <div class="message__loading-bar"></div>
+            </div>
+        </div>
+        <span onClick="copyMessageToClipboard(this)"
+        class="message__icon hide"><i class='bx bx-copy-alt'></i></span>    
+    
+    `;
+}
+
 
